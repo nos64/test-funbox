e2ebda12d7310a8ea37e972173364481243d495b
nos64 <manosov1984@gmail.com>
2023-01-23 12:30:24 +0400

    feat: add server address

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:3024';
-// export const API_URL = 'https://methed-burger-api.onrender.com';
+// export const BASE_URL = 'http://localhost:3024';
+export const BASE_URL = 'https://funbox-test-api.onrender.com';
 export const PREFIX_PRODUCT = '/api/product';
 
 export const titleText = 'Ты сегодня покормил кота?';
